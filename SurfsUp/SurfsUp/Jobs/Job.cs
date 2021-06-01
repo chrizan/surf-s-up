2fa3ce21036c76bb766541629011c439f145c572
Zanner Christoph <czanner@hamilton.ch>
2021-06-01 10:06:09 +0200

    Add Quartz as ASP.NET Core hosted service

---
+++ SurfsUp/SurfsUp/Jobs/Job.cs
@@ -0,0 +1,25 @@
+﻿using Quartz;
+using SurfsUp.DataProvider.Contract;
+using SurfsUp.DataProvider.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SurfsUp.SurfsUp.Jobs
+{
+    public class Job : IJob
+    {
+        private readonly IDataProvider _dataProvider;
+
+        public Job(IDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            SwellData data = await _dataProvider.GetSwellDataFromWeb("https://de.magicseaweed.com/Levanto-Surf-Report/3571/");
+            JobKey key = context.JobDetail.Key;
+            await Console.Error.WriteLineAsync($"Instance {key} of Job executed. Nonsense {data[1]}");
+        }
+    }
+}
--- SurfsUp/SurfsUp/Program.cs
+++ SurfsUp/SurfsUp/Program.cs
@@ -1,16 +1,22 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Quartz;
 using SurfsUp.DataProvider.Contract;
 using SurfsUp.DataProvider.Data;
-using SurfsUp.SurfsUp.Notifier;
+using SurfsUp.SurfsUp.Jobs;
+using System;
 
 namespace SurfsUp.SurfsUp
 {
     public class Program
     {
+        private static IConfiguration Configuration { get; set; }
+
         public static void Main(string[] args)
         {
+            Configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -22,8 +28,30 @@ namespace SurfsUp.SurfsUp
                 })
                 .ConfigureServices(services =>
                 {
-                    services.AddHostedService<NotifierService>();
-                    services.AddSingleton<IDataProvider, MswDataProvider>();
+                    //services.Configure<QuartzOptions>(Configuration.GetSection("Quartz"));
+
+                    services.AddQuartz(q =>
+                    {
+                        q.UseMicrosoftDependencyInjectionJobFactory();
+
+                        services.AddSingleton<IDataProvider, MswDataProvider>();
+
+                        var jobKey = new JobKey("jobKeyName", "jobKeyGroup");
+                        q.AddJob<Job>(jobKey, j => j.WithDescription("my awesome job"));
+
+                        q.AddTrigger(t => t
+                            .WithIdentity("Cron Trigger")
+                            .ForJob(jobKey)
+                            .StartAt(DateBuilder.EvenSecondDate(DateTimeOffset.UtcNow.AddSeconds(3)))
+                            .WithCronSchedule("0/3 * * * * ?")
+                            .WithDescription("my awesome cron trigger")
+                        );
+                    });
+
+                    services.AddQuartzHostedService(options =>
+                    {
+                        options.WaitForJobsToComplete = true;
+                    });
                 });
     }
 }
