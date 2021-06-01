bca284409748dd0848037613bd726e385f83c2f4
ChriZan <christophzanner@gmx.ch>
2021-06-01 21:06:45 +0200

    Move spots to appsettings

--- SurfsUp/SurfsUp/Jobs/Job.cs
+++ SurfsUp/SurfsUp/Jobs/Job.cs
@@ -1,8 +1,10 @@
-﻿using Quartz;
+﻿using Microsoft.Extensions.Configuration;
+using Quartz;
 using SurfsUp.DataProvider.Contract;
 using SurfsUp.DataProvider.Models;
 using SurfsUp.SurfsUp.Messengers;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SurfsUp.SurfsUp.Jobs
@@ -11,15 +13,23 @@ namespace SurfsUp.SurfsUp.Jobs
     {
         private readonly IDataProvider _dataProvider;
         private readonly IMessenger _messenger;
+        private readonly IConfiguration _configuration;
 
-        public Job(IDataProvider dataProvider, IMessenger messenger)
+        public Job(IConfiguration configuration, IDataProvider dataProvider, IMessenger messenger)
         {
             _dataProvider = dataProvider;
             _messenger = messenger;
+            _configuration = configuration;
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
+            var spots = _configuration.GetSection("MswSpots").GetChildren().ToDictionary(s => s.Key, s => s.Value);
+            foreach (var spot in spots)
+            {
+                string spotName = spot.Key;
+                string spotUrl = spot.Value;
+            }
             await _messenger.SendMessage();
             SwellData data = await _dataProvider.GetSwellDataFromWeb("https://de.magicseaweed.com/Levanto-Surf-Report/3571/");
             JobKey key = context.JobDetail.Key;
