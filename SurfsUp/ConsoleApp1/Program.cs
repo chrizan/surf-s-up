8f617667330b080b75582f63f33b3d721f1df159
Christoph Zanner <CZanner@Hamilton.ch>
2021-06-08 16:49:12 +0200

    Add test project

---
+++ SurfsUp/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using SurfsUp.DataProvider.Data;
+using SurfsUp.DataProvider.Models;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.hydrodaten.admin.ch/de/2018.html";
+
+            var provider = new BafuDataProvider();
+            BafuData bafuData = provider.GetOutflowData(url).Result;
+
+            Console.WriteLine($"Current Outflow is {bafuData.Outflow}m^3/s @ a temperature of {bafuData.DegreeCelsius}°");
+        }
+    }
+}
