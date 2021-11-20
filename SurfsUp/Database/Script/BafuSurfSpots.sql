1ea2480dddf442a9954100f989dc1dccf2079290
ChriZan <christophzanner@gmx.ch>
2021-11-20 21:22:59 +0100

    Add db scripts with default values

---
+++ SurfsUp/Database/Script/BafuSurfSpots.sql
@@ -0,0 +1,18 @@
+﻿INSERT INTO BafuSurfSpots
+("Url", "Name", Outflow)
+Values
+(
+'https://www.hydrodaten.admin.ch/de/2018.html',
+'Reuss',
+250
+),
+(
+'https://www.hydrodaten.admin.ch/de/2106.html',
+'Birs',
+70
+),
+(
+'https://www.hydrodaten.admin.ch/de/2181.html',
+'Thur',
+400
+)
---
+++ SurfsUp/Database/Script/MswSurfSpots.sql
@@ -0,0 +1,39 @@
+﻿INSERT INTO MswSurfSpots
+("Url", "Name", FullStars, BlurredStars)
+Values
+(
+'https://magicseaweed.com/Levanto-Surf-Report/3571/',
+'Levanto',
+1,
+0
+),
+(
+'https://magicseaweed.com/Forte-Dei-Marmi-Pontile-Surf-Report/3580/',
+'Forte Dei Marmi',
+1,
+0
+),
+(
+'https://magicseaweed.com/Ansedonia-La-Sinistra-Surf-Report/3586/',
+'Ansedonia',
+1,
+0
+),
+(
+'https://magicseaweed.com/Vieux-Boucau-Surf-Report/64/',
+'Vieux Boucau',
+3,
+0
+),
+(
+'https://magicseaweed.com/Gerra-Surf-Report/4393/',
+'Gerra',
+3,
+0
+),
+(
+'https://magicseaweed.com/Playa-de-Meron-Surf-Report/172/',
+'Playa de Meron',
+3,
+0
+)
